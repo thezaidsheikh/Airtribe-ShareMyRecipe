2604835e2fa9fb9876d5c3bb877a134a7355f93d
Zaid Sheikh <zaidsheikh530@gmail.com>
2025-11-07 22:22:45 +0530

    chore: First commit

---
+++ src/main/java/com/airtribe/sharemyrecipe/SharemyrecipeApplication.java
@@ -0,0 +1,13 @@
+package com.airtribe.sharemyrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharemyrecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharemyrecipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airtribe/sharemyrecipe/SharemyrecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airtribe.sharemyrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharemyrecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
